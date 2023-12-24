8af84df3495d749087212c95f821c757a6e0337b
Mthabs <>
2023-12-24 02:55:19 +0000

    add changes

--- src/pages/friends/FindFriends.js
+++ src/pages/friends/FindFriends.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import axios from '../../api/axiosDefaults';
+import axios from 'axios';
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
 
 const FindFriends = () => {
--- src/pages/friends/Followers.js
+++ src/pages/friends/Followers.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import axios from '../../api/axiosDefaults';
+import axios from 'axios';
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
 
 const Followers = () => {
