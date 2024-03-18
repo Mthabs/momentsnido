94456c4d0b90bd89b4d6957b6537f4e5181d7dcd
Thabani Nhleko <mthabs1@gmail.com>
2024-03-18 19:53:19 +0530

    Fixed routes created store

--- src/App.js
+++ src/App.js
@@ -3,44 +3,41 @@ import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
 import "./api/axiosDefaults";
-import SignUpForm from "./pages/auth/SignUpForm";
-import SignInForm from "./pages/auth/SignInForm";
-import PostCreateForm from "./pages/posts/PostCreateForm";
-import PostPage from "./pages/posts/PostPage";
-import PostsPage from "./pages/posts/PostsPage";
-import PhotoUploadForm from "./pages/photos/PhotoUploadForm";
-import PhotoPage from "./pages/photos/PhotoPage";
-import PhotosPage from "./pages/photos/PhotosPage"; 
-import VideosPage from "./pages/videos/VideosPage";
-import VideoUploadForm from "./pages/videos/VideoUploadForm";
-import VideoPage from "./pages/videos/VideoPage";
-import { useCurrentUser } from "./contexts/CurrentUserContext";
-import PostEditForm from "./pages/posts/PostEditForm";
-import PhotoEditForm from "./pages/photos/PhotoEditForm";
-import VideoEditForm from "./pages/videos/VideoEditForm";
-import ProfilePage from "./pages/profiles/ProfilePage";
-import UsernameForm from "./pages/profiles/UsernameForm";
-import UserPasswordForm from "./pages/profiles/UserPasswordForm";
-import ProfileEditForm from "./pages/profiles/ProfileEditForm";
-import Friends from './pages/friends/Friends';
+import SignUpForm from "./pages/auth/register/Register";
+import SignInForm from "./pages/auth/login/login";
+// import PostCreateForm from "./pages/posts/PostCreateForm";
+// import PostPage from "./pages/posts/PostPage";
+// import PostsPage from "./pages/posts/PostsPage";
+// import PhotoUploadForm from "./pages/photos/PhotoUploadForm";
+// import PhotoPage from "./pages/photos/PhotoPage";
+// import PhotosPage from "./pages/photos/PhotosPage"; 
+// import VideosPage from "./pages/videos/VideosPage";
+// import VideoUploadForm from "./pages/videos/VideoUploadForm";
+// import VideoPage from "./pages/videos/VideoPage";
+// import { useCurrentUser } from "./contexts/CurrentUserContext";
+// import PostEditForm from "./pages/posts/PostEditForm";
+// import PhotoEditForm from "./pages/photos/PhotoEditForm";
+// import VideoEditForm from "./pages/videos/VideoEditForm";
+// import ProfilePage from "./pages/profiles/ProfilePage";
+// import UsernameForm from "./pages/profiles/UsernameForm";
+// import UserPasswordForm from "./pages/profiles/UserPasswordForm";
+// import ProfileEditForm from "./pages/profiles/ProfileEditForm";
+// import Friends from './pages/friends/Friends';
 
 function App() {
-  const currentUser = useCurrentUser();
-  const profile_id = currentUser?.profile_id || "";
-
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
-          <Route
+          {/* <Route
             exact
             path="/"
             render={() => (
               <PostsPage message="No results found. Adjust the search keyword." />
             )}
-          />
-          <Route
+          /> */}
+          {/* <Route
             exact
             path="/feed"
             render={() => (
@@ -49,8 +46,8 @@ function App() {
                 filter={`owner__followed__owner__profile=${profile_id}&`}
               />
             )}
-          />
-          <Route
+          /> */}
+          {/* <Route
             exact
             path="/liked"
             render={() => (
@@ -59,10 +56,10 @@ function App() {
                 filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}
               />
             )}
-          />
+          /> */}
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
-          <Route exact path="/posts/create" render={() => <PostCreateForm />} />
+          {/* <Route exact path="/posts/create" render={() => <PostCreateForm />} />
           <Route exact path="/posts/:id" render={() => <PostPage />} />
           <Route exact path="/posts/:id/edit" render={() => <PostEditForm />} />
           <Route exact path="/profiles/:id" render={() => <ProfilePage />} />
@@ -72,8 +69,8 @@ function App() {
           <Route exact path="/videos/upload" render={() => <VideoUploadForm />} />
           <Route exact path="/videos/:id" render={() => <VideoPage />} />
           <Route exact path="/videos/:id/edit" render={() => <VideoEditForm />} />
-          <Route path="/friends" render={() => <Friends />} />
-          <Route
+          <Route path="/friends" render={() => <Friends />} /> */}
+          {/* <Route
             exact
             path="/profiles/:id/edit/username"
             render={() => <UsernameForm />}
@@ -87,7 +84,7 @@ function App() {
             exact
             path="/profiles/:id/edit"
             render={() => <ProfileEditForm />}
-          />
+          /> */}
 
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
