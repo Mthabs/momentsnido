c2859f6351bcd13c4e41f77f24f1e0e96bdd927e
Thabani Nhleko <mthabs1@gmail.com>
2024-03-20 13:34:39 +0530

    removed unnececcary fields

--- src/components/fields.js
+++ src/components/fields.js
@@ -1,4 +1,5 @@
-import  Form  from "react-bootstrap/Form";
+import { Form } from "react-bootstrap";
+
 
 export const renderTextField = ({
   input,
@@ -38,164 +39,31 @@ export const renderTextField = ({
   );
 };
 
-export const renderCheckBoxField = ({ input, label, meta }) => {
-  return (
-    <Form.Group className="my-1">
-      <Form.Control
-        type="checkbox"
-        id={input.name}
-        name={input.name}
-        onChange={input.onChange}
-        className={meta.touched ? (meta.error ? "is-invalid" : "is-valid") : ""}
-      />
 
-      <Form.Label
-        style={{ fontWeight: 400 }}
-        htmlFor={input.name}
-        className={
-          meta.touched && meta.error
-            ? "form-check-label mx-2 text-danger"
-            : "form-check-label mx-2"
-        }
-      >
-        {label}
-      </Form.Label>
+export const renderTextAreaField = ({ input,  meta, ...custom }) => {
+  return (
+    <Form.Group>
+      <Form.Control as="textarea" {...input} {...custom} />
+      {meta.touched && meta.error && <Form.Text className="text-danger">{meta.error}</Form.Text>}
     </Form.Group>
   );
 };
 
-export const renderSelectField = ({
-  input,
-  required,
-  label,
-  options,
-  help_text,
-  meta,
-  disabled,
-}) => {
-  return (
-    <Form.Group className="my-1">
-      <Form.Label
-        style={{ fontWeight: 500 }}
-        htmlFor={input.name}
-        className={
-          meta.touched && meta.error
-            ? "form-label text-left text-danger"
-            : "form-label text-left"
-        }
-      >
-        {label} {required && <span className="text-danger fw-bold">*</span>}
-      </Form.Label>
-      <Form.Control
-        type="select"
-        name={input.name}
-        id={input.named}
-        className={meta.touched ? (meta.error ? "is-invalid" : "is-valid") : ""}
-        onChange={input.onChange}
-        value={input.value}
-        disabled={disabled}
-      >
-        <option key="" value="">
-          Choose an Option
-        </option>
-        {options &&
-          options.map((item) => {
-            return (
-              <option key={item.value} value={item.value}>
-                {item.label}
-              </option>
-            );
-          })}
-      </Form.Control>
-      <div className="small">{help_text}</div>
-      {meta.touched && meta.error && (
-        <div className="text-danger">{meta.error}</div>
-      )}
-    </Form.Group>
-  );
-};
 
-export const renderIntegerField = ({
-  input,
-  required,
-  label,
-  placeholder,
-  help_text,
-  post,
-  pre,
-  meta,
-  disabled,
-}) => {
-  return (
-    <>
-      <Form.Group className="my-1">
-        <Form.Label
-          style={{ fontWeight: 500 }}
-          htmlFor={input.name}
-          className={
-            meta.touched && meta.error ? "form-label text-danger" : "form-label"
-          }
-        >
-          {label} {required && <span class="text-danger fw-bold">*</span>}
-        </Form.Label>
-        <div className="input-group">
-          {pre && <span className="input-group-text">{pre}</span>}
-          <Form.Control
-            type="number"
-            id={input.name}
-            name={input.name}
-            className={
-              meta.touched ? (meta.error ? "is-invalid" : "is-valid") : ""
-            }
-            placeholder={placeholder}
-            value={input.value}
-            onChange={input.onChange}
-            disabled={disabled}
-          />
-          {post && <span className="input-group-text">{post}</span>}
-        </div>
-        <div className="small">{help_text}</div>
-        {meta.touched && meta.error && (
-          <div className="text-danger">{meta.error}</div>
-        )}
-      </Form.Group>
-    </>
-  );
-};
-
-export const renderFileField = ({
-  input,
-  required,
-  label,
-  help_text,
-  meta,
-}) => {
-  return (
-    <>
-      <Form.Group
-        className={meta.touched && meta.error ? "my-1 text-danger" : "my-1"}
-      >
-        <Form.Label
-          style={{ fontWeight: 500 }}
-          htmlFor={input.name}
-          className="form-label"
-        >
-          {label} {required && <span className="text-danger fw-bold">*</span>}
-        </Form.Label>
-        <Form.Control
-          type="file"
-          id={input.name}
-          name={input.name}
-          onChange={input.onChange}
-        />
-        <div className="small">{help_text}</div>
-        {meta.touched && meta.error && (
-          <div className="text-danger">{meta.error}</div>
-        )}
-      </Form.Group>
-    </>
-  );
-};
+export const renderFileField = ({ 
+  input, label, meta, image, ...custom }) => {
+  return(
+    <Form.Group controlId={input.name} className="position-relative my-2">
+      <Form.Label><img src={image} alt="Upload" width="45" height="45" className="mr-2" />
+    </Form.Label>
+    <Form.Control
+      type="file"
+      name={input.name}
+      onChange={(e) => input.onChange(e.target.files[0])}
+      isInvalid={meta.touched && meta.error}
+    />
+  </Form.Group>
+)};
 
 export const renderEmailField = ({
   input,
@@ -271,40 +139,3 @@ export const renderPasswordField = ({
   );
 };
 
-export const renderDateField = ({
-  input,
-  meta,
-  label,
-  placeholder,
-  help_text,
-  required,
-}) => {
-  return (
-    <Form.Group className="my-1">
-      <Form.Label
-        style={{ fontWeight: 500 }}
-        htmlFor={input.name}
-        className={
-          meta.touched && meta.error
-            ? "form-label text-left text-danger"
-            : "form-label text-left"
-        }
-      >
-        {label} {required && <span className="text-danger fw-bold">*</span>}
-      </Form.Label>
-      <Form.Control
-        className={meta.touched ? (meta.error ? "is-invalid" : "is-valid") : ""}
-        type="date"
-        id={input.name}
-        name={input.name}
-        placeholder={placeholder}
-        value={input.value}
-        onChange={input.onChange}
-      />
-      <div className="small">{help_text}</div>
-      {meta.touched && meta.error && (
-        <div className="text-danger">{meta.error}</div>
-      )}
-    </Form.Group>
-  );
-};
