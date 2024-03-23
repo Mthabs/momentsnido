76254cdba7957fdf08642efac245b42586107afb
Thabani Nhleko <mthabs1@gmail.com>
2024-03-23 08:16:52 +0530

    add: added comment handler

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -1,18 +1,73 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Avatar from "../../components/Avatar";
 import { MoreDropdown } from "../../components/MoreDropdown";
-
-import styles from "../../styles/Comment.module.css";
+import CommentForm from "./CommentForm";
+import styles from "../../styles/Post.module.css";
+import { customaxios } from "../../api/axiosDefaults";
 
 
 const Comment = (props) => {
-  const {id} = props
-  const user = JSON.parse(sessionStorage.getItem("user"))
+  const { id  , user, comment, likes_count } = props
+  const currentUser = JSON.parse(sessionStorage.getItem("user"))
 
-  const [showEditForm, setShowEditForm] = useState(false);
+  const [isDifferent, setDifferent] = useState(true);
+  const[like, setLike] = useState(false)
+  const [editForm, setEditForm] = useState(false);
+
+  useEffect(()=>{
+    if(user.id === currentUser.id){
+      setDifferent(false);
+      initialLike();
+    }
+  },[user])
+
   const handleDelete = async () => {
-   
+    const response = await customaxios.delete("/post/comment/modify/"+id+"/")
+    if(response.status===204){
+      alert("Comment deleted successfully")
+      document.location.reload()
+    }
   };
+
+  const handleSubmit = async (values) => {
+    const formData = new FormData()
+    formData.append("comment", values.comment)
+    const response = await customaxios.patch("/post/comment/modify/"+id+"/", formData)
+    if(response.status === 206){
+      alert("Comment updated successfully")
+      document.location.reload()
+    }
+    
+  }
+
+  const handleLike = (value) => {
+    if(value){
+      customaxios.post("/post/comment/like/"+id+"/")
+    }
+    else{
+      customaxios.delete("/post/comment/like/"+id+"/")
+    }
+    setLike(value)
+    document.location.reload()
+  }
+
+  const initialLike = async() => {
+    // Create Initial Like api and Integrate here: gets user like this post from api
+    try{
+        const response = await customaxios.get("/post/comment/like/"+id+"/")
+        if(response.status === 200){
+            setLike(response.data.like);
+        }
+    }
+    catch(e){
+        console.log(e)
+    }
+    
+}
+
+  const hideEditForm = () =>{
+    setEditForm(false)
+  }
 
   return (
     <div style={{backgroundColor:"white"}}>
@@ -24,16 +79,21 @@ const Comment = (props) => {
         <div >
           <span className={styles.Owner}>{user.full_name}</span>
         </div>
-        { !showEditForm && (
-          <MoreDropdown
-            handleEdit={() => setShowEditForm(true)}
+          {!isDifferent &&<MoreDropdown
+            handleEdit={() => setEditForm(true)}
             handleDelete={handleDelete}
-          />
-        )}
+          />}
       </div>
-      <div className="mx-5 px-5">
-        <p>Hello There</p>
-      </div>
+      {!editForm && <div className=" row d-flex" >
+        <div className="col-md-1"></div>
+        <div className="col-md-9"> <p>{comment}</p></div>
+        <div className="col-sm">
+        {like && <i className={`fas fa-heart ${styles.Heart}`} onClick={()=>{handleLike(false)}} style={{marginTop:"-10px"}}/>}
+        {!like && <i className="far fa-heart" onClick={()=>{handleLike(true)}} style={{marginTop:"-10px"}} /> }
+        {likes_count}
+        </div>
+      </div>}
+      {editForm && <CommentForm message={comment} onSubmit={handleSubmit} hideEditForm={hideEditForm} />}
       <hr />
     </div>
   );
